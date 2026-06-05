634b4f9517fcb54b6880d2b232529eae200f1d5b
Lucas <luk8nhassil@gmail.com>
2026-06-04 21:40:44 -0300

    temp: debug keys

---
+++ src/app/api/debug-keys/route.ts
@@ -0,0 +1,6 @@
+﻿import { NextResponse } from "next/server";
+import { redis } from "@/lib/redis";
+export async function GET() {
+  const keys = await redis.keys("*");
+  return NextResponse.json({ keys });
+}
